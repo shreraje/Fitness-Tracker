dfb1b62d3a4cd4f4d417a7a3e72e6180f5c554f4
Rajendra Shrestha <rajendrashr@gmail.com>
2020-11-20 22:13:29 -0800

    Added routes for the index.html

--- routes/html.js
+++ routes/html.js
@@ -3,12 +3,16 @@ const path = require("path");
 
 
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  res.sendFile(path.join(__dirname, "../public/html/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
+  res.sendFile(path.join(__dirname, "../public/html/stats.html"));
 });
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/html/index.html"))
+})
 
 module.exports = router;
 
